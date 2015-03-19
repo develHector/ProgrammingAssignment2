6a89b1175ec723646da4c1364114f03f7fe2e8bf
Hector <devel.hector@hotmail.com>
2015-03-18 22:39:23 -0600

    Coded both functions for matrix caching assignment
    Coded OK both functions for matrix caching assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Put comments here that give an overall description of what your functions do
+## Regards! Hector
+## Define the local environment variables and functions necesarry for the Cache Matrix
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function ( x = matrix() ) {
+      
+  m <- NULL ;
+  
+  set <- function( y ) {
+    x <<- y ;
+    m <<- NULL ;
+  }
+  
+  get <- function() x ;
+  
+  setinverse <- function(mean) m <<- mean ;
+  getinverse <- function() m ;
+  
+  list( set = set, get = get, setinverse = setinverse, getinverse = getinverse ) ;
+  
 }
 
+## As the assignment commands, this fnctionfunction retursn a matrix that is the inverse of 'x' and caches it as possible
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function ( x = makeCacheMatrix(), ... ) {
+  
+  m <- x$getinverse() ;
+  if( !is.null( m ) ) 
+  {
+    message( "getting cached data" ) ;
+    return( m ) ;
+  }
+  else    
+  {  
+    data <- x$get() ;
+    m <- solve( data, ... ) ;
+    x$setinverse( m ) ;
+    message( "normally calculated" ) ;
+    return( m ) ;
+  }
+  
 }
